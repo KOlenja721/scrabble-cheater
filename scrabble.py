7496b6f5c97020c1e18b8a9dcd560cd1c4449c8a
Alice Chen <alicechen118@gmail.com>
2017-09-11 01:18:37 -0700

    Get scrabble rack letters from command line args

--- scrabble.py
+++ scrabble.py
@@ -1,7 +1,20 @@
+import argparse
+import sys
+
+# Step 1: construct a word list
 with open('sowpods.txt') as f:
     read_data = f.readlines()
 i = 0
 for word in read_data:
     read_data[i] = word.strip()
     i += 1
-print(read_data)
+
+# Step 2: get the rack
+parser = argparse.ArgumentParser()
+parser.add_argument('rack', nargs='?')
+args = parser.parse_args()
+if len(sys.argv) == 1:
+    print("Please supply some letters. Ex: 'python scrabble.py RSTLNEI'")
+    exit()
+rack = args.rack.lower()
+print(rack)

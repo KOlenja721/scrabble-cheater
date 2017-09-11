5f8a4eecef15994044c8c0425976444ecd240e86
Alice Chen <alicechen118@gmail.com>
2017-09-11 01:50:45 -0700

    Find valid words from rack

--- scrabble.py
+++ scrabble.py
@@ -6,7 +6,7 @@ with open('sowpods.txt') as f:
     read_data = f.readlines()
 i = 0
 for word in read_data:
-    read_data[i] = word.strip()
+    read_data[i] = word.lower().strip()
     i += 1
 
 # Step 2: get the rack
@@ -17,4 +17,16 @@ if len(sys.argv) == 1:
     print("Please supply some letters. Ex: 'python scrabble.py RSTLNEI'")
     exit()
 rack = args.rack.lower()
-print(rack)
+
+# Step 3: find valid words
+valid_words = list()
+for word in read_data:
+    rack_dup = rack
+    valid = True
+    for char in word:
+        if char in rack_dup:
+            rack_dup = rack_dup.replace(char, "", 1)
+        else:
+            valid = False
+    if valid:
+        valid_words.append(word)
